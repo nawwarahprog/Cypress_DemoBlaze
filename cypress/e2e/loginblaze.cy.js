34760d100863163672df3f8871417e0bf96f8a4a
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-01 14:15:23 +0800

    Update loginblaze.cy.js

--- cypress/e2e/loginblaze.cy.js
+++ cypress/e2e/loginblaze.cy.js
@@ -6,19 +6,20 @@ describe ('Sign up Demo Blaze', function () {
         cy.visit('https://www.demoblaze.com/index.html')
     })
 
-    it('Sign Up Demo Blaze', function () {
-        cy.get('#signin2').click()
-        cy.get('#sign-username').type('saiful')
-        cy.get('#sign-password').type('saiful123')
+    //it('Sign Up Demo Blaze', function () {
+        //cy.get('#signin2').click()
+        //cy.get('#sign-username').type('saiful')
+        //cy.get('#sign-password').type('saiful123')
 
-        cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+        //cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
 
-    })
+    //})
 
     it('Login Demo Blaze', function () {
         cy.get('#login2').click()
-        cy.get('#loginusername').type('saiful')
-        cy.get('#loginpassword').type('saiful123')
+        cy.get('#loginusername').type('nawa123')
+        cy.get('#loginpassword').type('nawa123')
+
         cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
 
     })
