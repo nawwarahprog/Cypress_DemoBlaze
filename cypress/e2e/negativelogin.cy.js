53b26cd2619e041c8ca935df0f3f8c84bc07c356
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-01 18:55:55 +0800

    Create negativelogin.cy.js

---
+++ cypress/e2e/negativelogin.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe ('Login Demo Blaze', function () {
+
+    it('Visit URL Demo Blaze', function() {
+        cy.visit('https://www.demoblaze.com/index.html')
+    })
+
+    it('Negative Login Demo Blaze', function () {
+        cy.get('#login2').click()
+        cy.get('#loginusername').type('nawa123').click()
+        cy.get('#loginpassword').type('nawa1').click()
+
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+
+    })
+
+
+    
+})
+    
