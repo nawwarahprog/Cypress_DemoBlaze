c5687d98629ae133c4804ebe78cd1dde776e54bf
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-01 16:59:12 +0800

    Create addtocartblaze.cy.js

---
+++ cypress/e2e/addtocartblaze.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe ('Add to cart', function () {
+
+    it('Visit URL Demo Blaze', function() {
+        cy.visit('https://www.demoblaze.com/index.html')
+    })
+
+    
+    it('Add to cart', function () {
+
+        cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click()
+        cy.get('.col-sm-12 > .btn').click()
+        
+
+
+    })
+
+
+
+
+})
