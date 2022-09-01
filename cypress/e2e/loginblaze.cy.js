b748cc98f93ada12b26c8c69bf9a23578a3ff6ac
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-01 13:34:23 +0800

    Update loginblaze.cy.js

--- cypress/e2e/loginblaze.cy.js
+++ cypress/e2e/loginblaze.cy.js
@@ -1,20 +1,26 @@
-/// <reference types = "cypress" />
+/// <reference types="cypress" />
 
-describe ('Sign up Demo Blaze', function (){
+describe ('Sign up Demo Blaze', function () {
 
     it('Visit URL Demo Blaze', function() {
         cy.visit('https://www.demoblaze.com/index.html')
     })
 
-    it('Sign Up Demo Blaze', function() {
+    it('Sign Up Demo Blaze', function () {
         cy.get('#signin2').click()
         cy.get('#sign-username').type('saiful')
         cy.get('#sign-password').type('saiful123')
 
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
-        
+
+    })
+
+
+    it('Login Demo Blaze', function () {
 
 
     })
+
     
 })
+    
