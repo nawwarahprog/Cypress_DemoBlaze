2237f4dfa8530c8ab674f11d80fdebf3e18113e3
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-02 11:51:54 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '3agfuv',
   reporter: 'cypress-mochawesome-reporter', 
   reporterOptions: {
     charts: true, 
