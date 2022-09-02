f8460f8b2882879dd891942e310c5d6ef99f18d2
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-02 09:47:55 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,7 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
