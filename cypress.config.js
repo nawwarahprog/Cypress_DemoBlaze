fc59ab0242a1e090e8047085b96e0a11f7c9f87e
nawwarahprog <nawwarahyusof226@gmail.com>
2022-09-02 09:40:20 +0800

    generate report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', 
+  reporterOptions: {
+    charts: true, 
+    reportPageTitle: 'report-cypressDemoBlaze',
+    embeddedScreenshot: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    video: false,
+    screenshotsFolder: 'images'
+  },
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
